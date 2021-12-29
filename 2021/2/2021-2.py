1f26ee1792703fce43b859b9646b96f769d5f544
barasher <arnaud.barasher@gmail.com>
2021-12-29 23:52:29 +0100

    2021-2 part2

--- 2021/2/2021-2.py
+++ 2021/2/2021-2.py
@@ -16,3 +16,18 @@ for action in actions:
 print(f"depth: {depth}, position: {position}")
 print(f"result1: {depth * position}")
 
+# actions = [["forward", "5"], ["down", "5"], ["forward", "8"], ["up", "3"], ["down", "8"], ["forward", "2"]]
+position, depth, aim = 0, 0, 0
+for action in actions:
+    if action[0] == "up":
+        aim -= int(action[1])
+    elif action[0] == "down":
+        aim += int(action[1])
+    elif action[0] == "forward":
+        position += int(action[1])
+        if aim != 0:
+            depth += (aim * int(action[1]))
+    else:
+        print(f"unsupported action: {action}")
+print(f"depth: {depth}, position: {position}, aim: {aim}")
+print(f"result1: {depth * position}")
