b76ecbf4aa4233f0e5d2c76e456e97a9495f08ac
barasher <arnaud.barasher@gmail.com>
2022-01-29 23:57:24 +0100

    2021-10 part2

--- 2021/10/2021-10.py
+++ 2021/10/2021-10.py
@@ -8,13 +8,14 @@ def load(file):
 
 matchs = {"[": "]", "(": ")", "{": "}", "<": ">"}
 scores = {")": 3, "]": 57, "}": 1197, ">": 25137}
+compl_scores = {")": 1, "]": 2, "}": 3, ">": 4}
 
 
 def match(o, c):
     return matchs[o] == c
 
 
-def score(l):
+def compile(l):
     stack = []
     for c in l:
         if c in ["(", "[", "{", "<"]:
@@ -22,16 +23,29 @@ def score(l):
         else:
             poped = stack.pop()
             if not match(poped, c):
-                return scores[c]
-    return 0
+                return (stack, scores[c])
+    return (stack, 0)
 
 
 def part1(file):
     with open(file) as f:
         res = 0
         for l in f.readlines():
-            res += score(l[:-1])
+            res += compile(l[:-1])[1]
         print(res)
 
+def part2(file):
+    with open(file) as f:
+        s=[]
+        for l in f.readlines():
+            stack, score = compile(l[:-1])
+            if score == 0 and len(stack)>0:
+                sum = 0
+                while len(stack) > 0:
+                    c = matchs[stack.pop()]
+                    sum = (sum * 5) + compl_scores[c]
+                s.append(sum)
+        s.sort()
+        print(s[int(len(s)/2)])
 
-part1("input.txt")
+part2("input.txt")
