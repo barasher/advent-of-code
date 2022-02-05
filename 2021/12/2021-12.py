37d1abbe8fe6a005cac1e042e2a69b89e487b84e
barasher <arnaud.barasher@gmail.com>
2022-02-05 23:11:51 +0100

    2021-12 part1 refactoring

--- 2021/12/2021-12.py
+++ 2021/12/2021-12.py
@@ -15,26 +15,30 @@ def load(file):
 
 
 def part1(file):
-    edges = load(file)
     paths = []
-    visited = []
-    explore(edges, "", "start", paths, visited)
+    explore(load(file), "", "start", paths, [])
     print(len(paths))
 
 
+def can_be_visited(node, visited_nodes):
+    if node.isupper():
+        return True
+    count = len([v for v in visited_nodes if v == node])
+    if count == 0:
+        visited_nodes.append(node)
+        return True
+    return False
+
+
 def explore(edges, cur_path, cur_node, paths, visited_nodes):
-    if cur_node.islower():
-        if cur_node in visited_nodes:
-            return
-        visited_nodes.append(cur_node)
+    if can_be_visited(cur_node, visited_nodes):
+        cur_path = f"{cur_path},{cur_node}"
 
-    cur_path = f"{cur_path},{cur_node}"
-
-    for next in edges[cur_node]:
-        if next == "end":
-            paths.append(f"{cur_path},end")
-            continue
-        explore(edges, cur_path, next, paths, visited_nodes[:])
+        for next in edges[cur_node]:
+            if next == "end":
+                paths.append(f"{cur_path},end")
+                continue
+            explore(edges, cur_path, next, paths, visited_nodes[:])
 
 
 part1("input.txt")
