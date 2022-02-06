02e65eb403f705d11c57bd464c0a9177f6acb944
barasher <arnaud.barasher@gmail.com>
2022-02-06 23:27:43 +0100

    2021-14 part2

--- 2021/14/2021-14.py
+++ 2021/14/2021-14.py
@@ -1,4 +1,5 @@
 # https://adventofcode.com/2021/day/14
+import math
 
 def load(file):
     with open(file) as f:
@@ -31,4 +32,31 @@ def part1(file, iter):
     print(counts[-1] - counts[0])
 
 
-part1("input.txt", 10)
+def part2(file, iter):
+    seq, trans = load(file)
+
+    duos = {}
+    for i in range(0, len(seq) - 1):
+        k = seq[i:i + 2]
+        duos[k] = duos.get(k, 0) + 1
+
+    for it in range(0, iter):
+        new = {}
+        for duo, count in duos.items():
+            k = duo[0] + trans[duo]
+            new[k] = new.get(k, 0) + count
+            k = trans[duo] + duo[1]
+            new[k] = new.get(k, 0) + count
+        duos = new
+
+    counts = {}
+    for duo, count in duos.items():
+        counts[duo[0]] = counts.get(duo[0], 0) + count
+        counts[duo[1]] = counts.get(duo[1], 0) + count
+
+    scores = [math.ceil(count/2) for count in counts.values()]
+    scores.sort()
+    print(scores[-1] - scores[0])
+
+
+part2("input.txt", 40)
